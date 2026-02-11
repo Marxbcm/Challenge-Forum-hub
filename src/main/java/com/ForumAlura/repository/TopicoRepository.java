91bbf2eaa9404d443bdffd69edf8baa36669e71b
Marxbcm <marxbeltra@gmail.com>
2026-02-11 11:08:00 -0300

    Update TopicoRepository.java

--- src/main/java/com/ForumAlura/repository/TopicoRepository.java
+++ src/main/java/com/ForumAlura/repository/TopicoRepository.java
@@ -1,4 +1,12 @@
 package com.ForumAlura.repository;
 
-public class TopicoRepository {
+import com.ForumHub.model.topico.Topico;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface TopicoRepository extends JpaRepository<Topico, Long> {
+
+    Page<Topico> findAll(Pageable paginacao);
 }
+
