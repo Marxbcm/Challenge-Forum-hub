312238efcb1c56812e51e96afe88b9bcaa73be52
Marxbcm <marxbeltra@gmail.com>
2026-02-11 10:28:27 -0300

    Update UsuarioController.java

--- src/main/java/com/ForumAlura/controller/UsuarioController.java
+++ src/main/java/com/ForumAlura/controller/UsuarioController.java
@@ -1,4 +1,46 @@
 package com.ForumAlura.controller;
 
+import br.alura.forumAlura.model.usuario.DadosAutenticacao;
+import br.alura.forumAlura.model.usuario.DadosListagemUsuario;
+import br.alura.forumAlura.model.usuario.Usuario;
+import br.alura.forumAlura.repository.UsuarioRepository;
+import io.swagger.v3.oas.annotations.security.SecurityRequirement;
+import jakarta.validation.Valid;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.web.PageableDefault;
+import org.springframework.http.ResponseEntity;
+import org.springframework.transaction.annotation.Transactional;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.util.UriComponentsBuilder;
+
+@RestController
+@SecurityRequirement(name = "bearer-key")
+@RequestMapping("/usuario")
 public class UsuarioController {
+
+    @Autowired
+    private UsuarioRepository repository;
+
+
+    @PostMapping
+    @Transactional
+    public ResponseEntity criarUsuario(@RequestBody @Valid DadosAutenticacao dados, UriComponentsBuilder uriBuilder) {
+
+        var usuario = new Usuario(dados);
+        repository.save(usuario);
+
+        var uri = uriBuilder.path("/usuario/{id}").buildAndExpand(usuario.getId()).toUri();
+
+        return ResponseEntity.created(uri).body("Usuario " + usuario.getLogin() + " criado!");
+    }
+
+    @GetMapping
+    public ResponseEntity<Page<DadosListagemUsuario>> listar(@PageableDefault(size = 10, sort = {"login"}) Pageable paginacao) {
+        var page = repository.findAll(paginacao).map(DadosListagemUsuario::new);
+        return ResponseEntity.ok(page);
+    }
+
 }
+
